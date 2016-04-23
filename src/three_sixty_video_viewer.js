d94db2eb5d139973fa808f90a9fd0cd96b002979
Thiago Pontes <github@thiago.me>
2016-04-23 01:44:40 -0400

    keep texture creation agnostic

--- src/three_sixty_video_viewer.js
+++ src/three_sixty_video_viewer.js
@@ -2,13 +2,13 @@ import utils from './utils'
 import ThreeSixtyRenderer from './three_sixty_renderer'
 import CameraControls from './camera_controls'
 import {
-    VideoTexture,
-    LinearFilter,
-    Scene,
-    Object3D,
-    Vector3,
-    PerspectiveCamera,
-    RGBFormat,
+  VideoTexture,
+  LinearFilter,
+  Scene,
+  Object3D,
+  Vector3,
+  PerspectiveCamera,
+  RGBFormat,
 } from 'threejs360'
 
 export class ThreeSixtyVideoViewer {
@@ -21,29 +21,33 @@ export class ThreeSixtyVideoViewer {
     this.scene = this.createScene();
     this.scene.add(this.camera);
     this.controls = new CameraControls(this.camera, this.renderer.renderer.domElement);
-    this.createVideoElement(this.createTexture.bind(this));
+    this.createTexture();
+  }
+
+  createTexture() {
+    this._createVideoElement((el) => this._createVideoTexture(el));
   }
 
   render() {
-      var loop = () => {
-          this.controls.update();
-          this.renderer.render(this.scene, this.camera);
-          requestAnimationFrame(loop);
-      };
-      loop();
+    var loop = () => {
+      this.controls.update();
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(loop);
+    };
+    loop();
   }
 
-  createVideoElement(cb) {
-      let el = document.createElement('video');
-      this.el = el;
-      el.src = this.source;
-      el.loop = this.loop || false;
-      el.setAttribute('crossorigin', 'anonymous');
-      el.addEventListener('canplaythrough', () => cb(el));
-      el.addEventListener('error', () => this.onError());
+  _createVideoElement(cb) {
+    let el = document.createElement('video');
+    el.src = this.source;
+    el.loop = this.loop || false;
+    el.setAttribute('crossorigin', 'anonymous');
+    el.autoplay = "true";
+    el.addEventListener('canplaythrough', () => cb(el));
+    el.addEventListener('error', () => this.onError());
   }
 
-  createTexture(el) {
+  _createVideoTexture(el) {
     var texture = new VideoTexture(el);
     texture.minFilter = LinearFilter;
     texture.magFilter = LinearFilter;
@@ -63,8 +67,7 @@ export class ThreeSixtyVideoViewer {
     return scene;
   }
 
-  onError(err) {
-      console.log('error loading video');
-      console.log(err);
+  onError() {
+    console.log('error loading video');
   }
 }
