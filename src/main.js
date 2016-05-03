f4a55a246df3602bae003adce58729c005886915
Thiago Pontes <github@thiago.me>
2016-05-03 11:44:32 -0400

    only expose the video viewer for now

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 
 import {ThreeSixtyVideoViewer as Video} from './three_sixty_video_viewer'
-import {ThreeSixtyImageViewer as Image} from './three_sixty_image_viewer'
 
-export {Video, Image};
+export {Video};
 
