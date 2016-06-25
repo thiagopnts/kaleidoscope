d6ef7818fe3bda29736e96b57e9ac569f7691d45
Thiago Pontes <github@thiago.me>
2016-06-25 01:11:47 -0400

    use the canvas implementation for IE and Edge

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,6 @@ export default {
     return /iphone.*(7|8|9)_[0-9]/i.test(navigator.userAgent);
   },
   isIE() {
-    return /Trident/i.test(navigator.userAgent);
+    return /Trident|Edge/i.test(navigator.userAgent);
   }
 }
