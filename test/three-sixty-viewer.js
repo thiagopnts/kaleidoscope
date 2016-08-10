43299af208846fa721ebacc129825995e912a987
Thiago Pontes <github@thiago.me>
2016-08-10 18:02:53 -0400

    add some tests

--- test/three-sixty-viewer.js
+++ test/three-sixty-viewer.js
@@ -2,12 +2,13 @@ import ThreeSixty from '../src/three-sixty-viewer';
 
 describe('360 viewer', () => {
 
-  it('has a default height/width', () => {
+  it('has a default height/width and vertical panning', () => {
     let viewer = new ThreeSixty();
     assert.isDefined(viewer.height);
     assert.isNumber(viewer.height);
     assert.isDefined(viewer.width);
     assert.isNumber(viewer.width);
+    assert(viewer.verticalPanning);
   });
 
   it('controls should start looking at 90 degrees', () => {
@@ -28,8 +29,15 @@ describe('360 viewer', () => {
     viewer.controls.phi = 0.78;
     viewer.centralize();
     setTimeout(() => {
-        assert.equal(viewer.controls.phi.toFixed(2), 0);
-        assert.equal(viewer.controls.theta.toFixed(2), 3.14);
+      assert.equal(viewer.controls.phi.toFixed(2), 0);
+      assert.equal(viewer.controls.theta.toFixed(2), 3.14);
     }, 0);
+  });
+
+  it('disables vertical panning', () => {
+    let viewer = new ThreeSixty({verticalPanning: false});
+    viewer.controls.onMouseMove({clientX: 123, clientY: 123});
+    assert.equal(viewer.controls.phi, 0);
+    assert.equal(viewer.controls.theta.toFixed(2), 1.57);
   });
 });
