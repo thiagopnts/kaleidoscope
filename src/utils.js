c9a5a6f52f90b17c901009d2c5e2a8d1d3bc9531
Thiago Pontes <github@thiago.me>
2016-10-10 18:04:27 -0400

    only use audio driver for webviews on iphone

--- src/utils.js
+++ src/utils.js
@@ -3,9 +3,9 @@ export default {
     return /(ipad|iphone|ipod)/ig.test(navigator.userAgent);
   },
   shouldUseAudioDriver() {
-    let isOldiOS = /iphone.*(7|8|9)_[0-9]/i.test(navigator.userAgent);
-    let isWebView = /(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(navigator.userAgent);
-    return isOldiOS || isWebView;
+    let isOldiOSOnIphone = /iphone.*(7|8|9)_[0-9]/i.test(navigator.userAgent);
+    let isWebView = /(iPhone|iPod).*AppleWebKit(?!.*Safari)/i.test(navigator.userAgent);
+    return isOldiOSOnIphone || isWebView;
   },
   shouldUseCanvasInBetween() {
     return /trident|edge/i.test(navigator.userAgent);
