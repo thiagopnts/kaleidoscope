b555083abc6f4890dc00aa0da07cce55416acabd
Thiago Pontes <github@thiago.me>
2017-07-24 14:41:40 -0400

    decrease bundle size

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,15 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import strip from 'rollup-plugin-strip';
 
 
+const targets = [
+    {dest: 'dist/kaleidoscope.js', format: 'umd'},
+    {dest: 'dist/kaleidoscope.iife.js', format: 'iife'},
+];
+
+if (process.env.BABEL_ENV !== 'production') {
+  targets.push({dest: 'dist/kaleidoscope.es.js', format: 'es'})
+}
+
 export default {
   entry: 'src/main.js',
   plugins: [
@@ -17,9 +26,5 @@ export default {
   ],
   moduleName: 'Kaleidoscope',
   amd: { id: 'kaleidoscope' },
-  targets: [
-    {dest: 'dist/kaleidoscope.js', format: 'umd'},
-    {dest: 'dist/kaleidoscope.iife.js', format: 'iife'},
-    {dest: 'dist/kaleidoscope.es.js', format: 'es'},
-  ],
+  targets: targets,
 };
